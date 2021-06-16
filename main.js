6ab3ac6aff7c105dbe8ce82870740acb5fc9eea4
Florencia Martinez <florenciamart23@gmail.com>
2021-06-16 19:16:25 -0300

    agregue barra movil y comence con soy Lucas

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var altura = $('.soyLucas').offset().top;
+    altura = altura - 200
+
+
+
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > altura) {
+            $('.menuBlock2').addClass('menuFixed')
+            $('.menuBlock').addClass('menuNone');;
+        } else {
+            $('.menuBlock').removeClass('menuNone');
+            $('.menuBlock2').removeClass('menuFixed')
+        }
+    });
+
+});
